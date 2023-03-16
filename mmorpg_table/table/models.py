2d43acf62c4b9fbf81b9b4611278e6a3e50e6ea1
Bakiev75_New <bakiev75@mail.ru>
2023-03-16 14:02:21 +0500

    Добавление 3-х моделей - Categories, Ads, Reviews. Для пользователей используется стандартная модель User фреймворка.

--- mmorpg_table/table/models.py
+++ mmorpg_table/table/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    """Класс Categories для создания экземпляров категорий, к которым относятся объявления"""
+    categories = models.CharField(max_length=30)                                        # Категория (10 шт)
+
+
+class Ads(models.Model):
+    """Класс Ads для создания экземпляров объявлений"""
+    date_time_generation = models.DateTimeField(auto_now_add=True)                      # Дата и время создания
+    header = models.CharField(max_length=250)                                           # Заголовок
+    text_and_multimedia = models.TextField()                           # !!!!! Требуется замена на WYSIWYG - поле
+    id_users = models.ForeignKey(User, on_delete=models.CASCADE)                       # id автора объявления
+    id_categories = models.ForeignKey(Categories, on_delete=models.CASCADE)             # id категории объявления
+
+
+class Reviews(models.Model):
+    """Класс Reviews для создания экземпляров откликов пользователей на объявления"""
+    text = models.TextField()                                                           # текст комментария
+    id_users_rev = models.ForeignKey(User, on_delete=models.CASCADE)                    # id автора комментария
+    id_ads = models.ForeignKey(Ads, on_delete=models.CASCADE)                           # id статьи
+    rev_status = models.BooleanField(default=False)                                     # принят или нет отзыв
